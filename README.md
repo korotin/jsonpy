2e7102f4d0fe975d8a71820ea840272f5c6759ba
Martin Stolz <herr.offizier@gmail.com>
2012-08-17 16:49:44 +0400

    added promise-like pattern support

---
+++ README.md
@@ -0,0 +1,79 @@
+# jsonpy
+
+**jsonpy** is a tiny library for work with JSONP. Callback mechanism based on the CommonJS Promises/A pattern, so you can provide multiple callbacks for any possible state.
+
+## Usage
+
+The simpliest example of usage is calling **jsonpy** like an ordinary function:
+``` javascript
+// Single callback
+jsonpy('url', function(response) {
+	// process data
+});
+
+// ...with error handling
+jsonpy('url', function(response) {
+	// process data
+}, function() {
+	// handle error
+});
+
+// ...add always callback
+jsonpy('url', function(response) {
+	// process data
+}, function() {
+	// handle error
+}, function() {
+	// this code will be executed always
+});
+```
+
+But hey, what if we want to customize something? We may pass to **jsonpy** an object:
+``` javascript
+jsonpy({
+	// JSONP url.
+	// Required.
+	url: 'url',
+
+	// If the name of parameter containing callback name differs from 'callback',
+	// you can customize it by setting 'field' option.
+	// Optional.
+	field: 'jsonCallback',
+
+	// You can set a timeout for request in milliseconds.
+	// If request took more time than set in 'timeout', request will be aborted and
+	// fail callbacks will be invoked.
+	// By default timeout is disabled.
+	// Optional.
+	timeout: 5000,
+
+	// Typical set of callbacks.
+	// Only success callback is required. 
+	done: function(response) {
+		// process data
+	},
+	fail: function(response) {
+		// handle error
+	},
+	always: function() {
+		// this code will be executed always
+	}
+});
+```
+
+And now, the sweetest part. You can use promise-like chainable calls to provide multiple callbacks:
+``` javascript
+jsonpy({
+	url: 'url',
+	timeout: 5000
+}).done(function(response) {
+	// process data
+}).done(function(response) {
+	// process data again
+})
+.fail(function(response) {
+	// handle error
+}).always(function() {
+	// this code will be executed always
+});
+```
--- jsonpy.js
+++ jsonpy.js
@@ -1,46 +1,111 @@
-function jsonpy(o) {
-	
-	var name 	= '__jsonp'+parseInt(Math.random() * 10000),
+function jsonpy(o, d, f, a) {
+	// random callback name in window scope
+	var name 	= '__jsonp' + parseInt(Math.random() * 10000),
+	// url for request
+	url 		= typeof o === 'string' ? o : o.url,
+	// url parameter name containing callback name ('callback' usually)
 	field 		= o.field || 'callback',
-	params		= o.params || {},
-	url 		= o.url,
+	// timeout for request (no timeout by default)
 	timeout		= o.timeout || false,
 	timeoutId	= null,
-	done		= false,
 	
 	script 		= document.createElement('script'),
 	
-	buildUrl = function(url, params) {
-		var key = null, pairs = [];
-		params = params || {};
-		for (key in params) {
-			pairs.push(encodeURIComponent(key) + '=' + encodeURIComponent(params[key]));
+	// execution status: null if not executed, 'done' or 'fail'
+	status		= null,
+	// arguments for callbacks
+	cbArgs		= null,
+	// callbacks collection
+	callbacks	= {done: [], fail: [], always: []},
+	
+	// promise object returned by jsonpy
+	promise = {
+		done: function(callback) {
+			callbacks.done.push(callback);
+			runCallbacks();
+
+			return promise;
+		},
+		
+		fail: function(callback) {
+			callbacks.fail.push(callback);
+			runCallbacks();
+			
+			return promise;
+		},
+		
+		always: function(callback) {
+			callbacks.always.push(callback);
+			runCallbacks();
+			
+			return promise;
 		}
-		
-		if (!pairs.length) return url;
-		
-		return url + (url.indexOf('?') >= 0 ? '&' : '?') + pairs.join('&');
 	},
 	
+	// helper function, run all callbacks in array with given args
+	runCallbacksFromArray = function(cbs, args) {
+		while (cb = cbs.shift()) {
+			cb.apply(this, args);
+		}
+	},
+	
+	// run all callbacks for current status; called by promise methods and setStatus()
+	runCallbacks = function() {
+		if (!status) return;
+		
+		runCallbacksFromArray(callbacks[status], cbArgs);
+		runCallbacksFromArray(callbacks.always, cbArgs);
+	},
+	
+	// set status to done or fail and set callback args
+	// function may be called only once
+	setStatus = function(success, args) {
+		if (status) return;
+		
+		status = ['fail', 'done'][+success];
+		cbArgs = args;
+		runCallbacks();
+	},
+	
+	
+	
+	// build url for request: simply add callback parameter to url
+	buildUrl = function() {
+		return url 
+				+ ['?', '&'][+(url.indexOf('?') >= 0)]
+				+ [encodeURIComponent(field), encodeURIComponent(name)].join('=');
+	},
+	
+	// initialize jsonpy
 	init = function() {
-		params[field] 	= name;
-		url 			= buildUrl(url, params);
-		
 		script.type		= 'text/javascript';
-		script.src		= url;
+		script.src		= buildUrl();
+		script.async	= true;
+		
 		script.addEventListener('error', error, true);
+
+		if (o.done) promise.done(o.done);
+		if (o.fail) promise.done(o.fail);
+		if (o.always) promise.done(o.always);
+
+		if (d) promise.done(d);
+		if (f) promise.done(f);
+		if (a) promise.done(a);
 	},
 	
+	// perform request
 	connect = function() {
 		window[name] = success;
 		document.body.appendChild(script);
-		
 		if (timeout) {
 			timeoutId = setTimeout(error, timeout);
 		}
 	},
 	
+	// close connection and cleanup
 	close = function() {
+		if (status) return;
+		
 		delete window[name];
 		document.body.removeChild(script);
 		
@@ -48,28 +113,28 @@ function jsonpy(o) {
 			clearTimeout(timeoutId);
 			timeoutId = null;
 		}
-		
-		done = true;
 	},
 	
+	// called on request success
 	success = function() {
-		if (done) return;
+		if (status) return;
 		close();
-		
-		o.success.apply(this, arguments);
+		setStatus(true, arguments);
 	},
 	
+	// called on request error or timeout (if exists)
 	error = function() {
-		if (done) return;
+		if (status) return;
 		close();
-		
-		if (typeof o.error === 'function') {
-			o.error();
-		}
+		setStatus(false, arguments);
 	};
 	
-	
+
 	
 	init();
 	connect();
+		
+		
+		
+	return promise;
 }
